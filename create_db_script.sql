6d36f6b9e23eda188f971de190fb14a2d3342a38
Rahul Madduluri <rahulmadduluri@gmail.com>
2017-09-04 22:44:21 -0700

    fix errors in schmea v0

--- create_db_script.sql
+++ create_db_script.sql
@@ -1,52 +1,50 @@
 CREATE TABLE IF NOT EXISTS users (
-	id 				int 		NOT NULL AUTO_INCREMENT,
-	uuid			string		NOT NULL,
-	name 			string 		NOT NULL,
-	mobile_number	string		NOT NULL,
-	photo_url 		string,
-	date_created	timestamp	NOT NULL,
+	id 				int 			NOT NULL AUTO_INCREMENT,
+	uuid			varchar(36)		NOT NULL,
+	name 			varchar(200)	NOT NULL,
+	mobile_number	varchar(36)		NOT NULL,
+	photo_url 		varchar(200),
+	date_created	timestamp		NOT NULL,
 	UNIQUE KEY (mobile_number),
 	UNIQUE KEY (uuid),
 	PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE IF NOT EXISTS tags (
-	id 				int 		NOT NULL AUTO_INCREMENT,
-	uuid			string		NOT NULL,
-	name 			string 		NOT NULL,
-	is_thread 		boolean		NOT NULL,
-	is_topic		boolean		NOT NULL,
-	date_created	timestamp	NOT NULL,
+	id 				int 			NOT NULL AUTO_INCREMENT,
+	uuid			varchar(36)		NOT NULL,
+	name 			varchar(200)	NOT NULL,
+	is_thread 		boolean			NOT NULL,
+	is_topic		boolean			NOT NULL,
+	date_created	timestamp		NOT NULL,
 	UNIQUE KEY (uuid),
 	PRIMARY KEY (id)
-)
+);
 
 CREATE TABLE IF NOT EXISTS messages (
-	id  			int 		NOT NULL AUTO_INCREMENT,
-	uuid			string		NOT NULL,
-	is_thread 		boolean,	NOT NULL,
-	thread_id		int
-	is_text 		boolean		DEFAULT true,
-	full_text		string,
-	is_link			boolean,
-	link 			string,
-	date_created	timestamp	NOT NULL,
-	sender_id		int 		NOT NULL,
+	id  			int 			NOT NULL AUTO_INCREMENT,
+	uuid			varchar(36)		NOT NULL,
+	is_text 		boolean			DEFAULT true,
+	full_text		varchar(1000),
+	is_link			boolean			DEFAULT false,
+	link 			varchar(200),
+	date_created	timestamp		NOT NULL,
+	sender_id		int 			NOT NULL,
 	UNIQUE KEY (uuid),
 	PRIMARY KEY (id),
-	FOREIGN KEY	(sender_id) REFERENCES users.id
-)
+	FOREIGN KEY	(sender_id) REFERENCES users (id)
+);
 
 CREATE TABLE IF NOT EXISTS users_messages (
-	user_id 	int,	NOT NULL,
-	message_id	int,	NOT NULL,
-	FOREIGN KEY (user_id) REFERENCES users.id,
-	FOREIGN KEY (message_id) REFERENCES messages.id
-)
+	user_id 	int	NOT NULL,
+	message_id	int	NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users (id),
+	FOREIGN KEY (message_id) REFERENCES messages (id)
+);
 
 CREATE TABLE IF NOT EXISTS tags_messages (
-	tag_id 		int,	NOT NULL,
-	message_id	int,	NOT NULL,
-	FOREIGN KEY (tag_id) REFERENCES tags.id,
-	FOREIGN KEY (message_id) REFERENCES messages.id
-)
+	tag_id 		int	NOT NULL,
+	message_id	int	NOT NULL,
+	FOREIGN KEY (tag_id) REFERENCES tags (id),
+	FOREIGN KEY (message_id) REFERENCES messages (id)
+);
